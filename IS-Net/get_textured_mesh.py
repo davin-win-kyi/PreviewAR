63def843f7604b9e9a657bc5627c6fceefbd5a90
davin123 <davinkyi@gmail.com>
2025-11-18 15:27:47 -0800

    adding final pipeline completion

--- IS-Net/get_textured_mesh.py
+++ IS-Net/get_textured_mesh.py
@@ -73,23 +73,69 @@ def main():
 
     # input the image as well as the test.png file name into the method call image_post_processing.py
     # and call the method 
-    image_to_process = "test.png"
+    # image_to_process = "test.png"
 
     # here is the call to the post_processing_pipeline
     """
     TODO: put the variables you had from the main in image_post_processing.py here
     and also put in the right parameters into image_post_processing
     """
-    image_post_processing()
+        # Object detection variables: object_detection.py
+    image_path = "previewar_test#1.jpg"
+    object_detection_json_name = "previewar_test#1_yolo11_o365.json"
+    object_detection_debug = "previewar_test#1_yolo11_o365.jpg"
+
+    # Target object variables: Target_object.py
+    product_url = (
+        "https://www.amazon.com/MODNEST-Modular-Sectional-Boneless-Assembly/dp/"
+        "B0FCYC86TT/ref=sr_1_5_sspa?crid=3RIV4C6CTWYQA&dib=eyJ2IjoiMSJ9."
+        "mCR5SjVr3IuoofQI97UxVmePO3nKmQbrqGkH6q7BhCWvXZfA2gaJDgyWsF100Jp3IznRSrEL8WKrwF2Xtlr-Q6YdVwugk_h-"
+        "vhluo-EvhxJBqShb2gctTmjV71AXRyHOoPE6xC5K1iS8ITO3gdrhSf93HanYw7yk5iuIDU0gQFvfQLiPHo05ZX5PuYYk5As943eAeCxhe_d7i07UPtixVaCT_4yDty6lWukpMHvQmtssdgjG_"
+        "zKvPqz8uqzZ5oAjIljfU3T1fj2lJKgKnqjlWxjA504G0RVwfRlQuUKr5bM.bAdATDJIW3OxMs6M16JiHRG9zAvYg7_B-SoRvPV4i5s&dib_tag=se&keywords=couch&qid=1762565138&"
+        "sprefix=cou%2Caps%2C264&sr=8-5-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1"
+    )
+    target_object_json = "previewar_test#1_yolo11_o365_merged_alias.json"
+    output_directory = "crops"
+
+    # Grounded SAM variables: mask_generation.py
+
+
+    # Masked image variables: mask_image.py
+    grounded_sam_segmented_image = "/Users/davinwinkyi/PreviewAR-V2/PreviewAR/best_image_post_processing/output/kept_with_white_bg.png"
+
+    # Yolo Mask variables: get_object_img_masks.py
+    yolo_masks_directory = "object_white_masks"
+    image_padding = 0
+
+    # Combined Mask variable: combine_two_mask.py
+    grounded_sam_output_directory = "output"
+    nana_banana_generation_mask = "mask_non_overlapping.png"
+
+    # Nano banana variables: Nano_banana.py
+    nano_banana_image_path = "uploads/inpainted_manual_mask.png"
+
+    image_post_processing(
+        image_path=image_path, 
+        object_detection_json_name=object_detection_json_name, 
+        object_detection_debug=object_detection_debug, 
+        product_url=product_url,
+        target_object_json=target_object_json, 
+        output_directory=output_directory,
+        grounded_sam_segmented_image=grounded_sam_segmented_image,
+        yolo_masks_directory=yolo_masks_directory,
+        image_padding=image_padding,
+        grounded_sam_output_directory=grounded_sam_output_directory,
+        nana_banana_generation_mask=nana_banana_generation_mask,
+        nano_banana_image_path=nano_banana_image_path
+    )
     
-
-    post_processed_image = "output/post_processing_image.png"
-
+    post_processed_image = "uploads/post_processing_image.png"
 
     # setting arguments for later use
     ap = argparse.ArgumentParser(description="Drive Hunyuan3D Gradio with Selenium and fetch textured mesh.")
     ap.add_argument("--url", default="http://localhost:1080//", help="Gradio URL")
-    ap.add_argument("--image", default="test.png", help="Path to input image")
+    # ap.add_argument("--image", default="test.png", help="Path to input image")
+    ap.add_argument("--image", default=post_processed_image, help="Path to input image")
     ap.add_argument("--cache-dir", default=r"C:\Users\davin\OneDrive\Documents\HY3D2\Hunyuan3D2_WinPortable_cu126\Hunyuan3D2_WinPortable\Hunyuan3D-2-vanilla\gradio_cache",
                     help="Path to Hunyuan3D gradio_cache directory")
     ap.add_argument("--timeout", type=int, default=120, help="Max seconds to wait for textured mesh")
